1ac42972022a589297476d55690dd02a83778cd6
Lachlan Sylvester <lachlan.sylvester@hypothetical.com.au>
2009-02-15 11:52:20 +1100

    initial commit

---
+++ template.rb
@@ -0,0 +1,25 @@
+gem 'RedCloth', :lib => 'redcloth'
+gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+# gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com'
+gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
+gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
+gem 'haml'
+
+rake "gems:install"
+plugin 'footnotes', :git => 'git://github.com/josevalim/rails-footnotes.git'
+run 'touch test/factory.rb'
+
+git :init
+
+file ".gitignore", <<-END
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+END
+
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/example_database.yml"
+
+git :add => '.', :commit => "-m 'initial commit'"
